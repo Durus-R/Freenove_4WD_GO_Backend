75b67ba1eedf5f7293273554630b6b9288dabec8
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-08-12 23:26:55 +0200

    Update protoc.sh to include Go PATHs
    This diff modifies the protoc.sh file to explicitly add paths for the Go language binaries. This ensures that the appropriate versions of Go and gRPC tools are utilized during protocol buffer compilation, especially in environments where multiple versions of Go might be installed.

--- protoc.sh
+++ protoc.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 mkdir -p dist
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/go/bin
 protoc --go_out=dist/ \
   --go_opt=paths=source_relative \
   --go-grpc_out=dist/ \
