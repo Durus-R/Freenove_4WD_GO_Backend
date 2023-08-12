502b5e98467b99a6ee7ec772381732c657150e93
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-08-12 23:25:14 +0200

    "Refactor ADCServer to handle errors
    ADCServer functions have been refactored, to handle and return potential errors from ADC methods. Instead of assuming the ADC methods (Battery and IDR) always succeed, we now catch and return any errors they might produce. This gives consumers of the RPC service more information to deal with potential failures, improving reliability and debuggability of the system."

--- rpc/adc.go
+++ rpc/adc.go
@@ -9,20 +9,20 @@ import (
 
 type ADCServer struct {
 	pb.UnimplementedADCServer
-	ADC car.ADC
+	ADC *car.ADC
 }
 
-func (s ADCServer) Battery(ctx context.Context, _ *empty.Empty) (*pb.BatteryState, error) {
-	batteryInPercent := s.ADC.Battery()
+func (s *ADCServer) Battery(_ context.Context, _ *empty.Empty) (*pb.BatteryState, error) {
+	batteryInPercent, err := s.ADC.Battery()
 
-	return &pb.BatteryState{Loaded: float32(batteryInPercent)}, nil
+	return &pb.BatteryState{Loaded: float32(batteryInPercent)}, err
 }
 
-func (s ADCServer) IDR(ctx context.Context, _ *empty.Empty) (*pb.IDRState, error) {
-	idrMeasure := s.ADC.IDR()
+func (s *ADCServer) IDR(_ context.Context, _ *empty.Empty) (*pb.IDRState, error) {
+	idrMeasure, err := s.ADC.IDR()
 
 	return &pb.IDRState{
 		Left:  float32(idrMeasure[0]),
 		Right: float32(idrMeasure[1]),
-	}, nil
+	}, err
 }
