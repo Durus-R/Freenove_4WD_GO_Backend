7dd44e3630906aa2d07a8c72ba21fd27ccb776ad
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-08-12 23:25:32 +0200

    "Add BuzzerServer for song playback control
    New file 'buzzer.go' was created, introducing a new type 'BuzzerServer' that interacts with the Car's Buzzer functionality. This provides RPC endpoints for operations like toggling buzzer state, setting BPM, calculating song duration, parsing, playing, and stopping songs. It greatly enhances the system functionality by allowing remote control of song playback via the RPC service."

---
+++ rpc/buzzer.go
@@ -0,0 +1,120 @@
+package rpc
+
+import (
+	car "Freenove_4WD_GO_Backend/Car"
+	pb "Freenove_4WD_GO_Backend/dist/proto"
+	"context"
+	"github.com/golang/protobuf/ptypes/empty"
+	"log"
+	"strings"
+)
+
+type BuzzerServer struct {
+	pb.UnimplementedBuzzerServer
+	buzz         *car.Buzzer
+	currentSong  car.Song
+	signalFinish chan struct{}
+	isClosing    bool
+}
+
+func (s *BuzzerServer) On(context.Context, *empty.Empty) (*empty.Empty, error) {
+	err := s.buzz.On()
+	return nil, err
+}
+
+func (s *BuzzerServer) Off(context.Context, *empty.Empty) (*empty.Empty, error) {
+	err := s.buzz.Off()
+	return nil, err
+}
+
+func (s *BuzzerServer) Toggle(context.Context, *empty.Empty) (*empty.Empty, error) {
+	err := s.buzz.Toggle()
+	return nil, err
+}
+
+func (s *BuzzerServer) SetBPM(_ context.Context, bpm *pb.SetBPMRequest) (*empty.Empty, error) {
+	err := s.buzz.SetBPM(float64(bpm.GetBpm()))
+	return nil, err
+}
+
+func (s *BuzzerServer) GetBPM(context.Context, *empty.Empty) (*pb.GetBPMRequest, error) {
+	return &pb.GetBPMRequest{Bpm: float32(s.buzz.GetBPM())}, nil
+}
+
+func (s *BuzzerServer) CalculateDuration(_ context.Context, sng *pb.Song) (*pb.SongDuration, error) {
+	song := importSong(sng)
+	dur := song.EstimatedDuration(s.buzz.GetBPM())
+	return &pb.SongDuration{Length: float32(dur)}, nil
+
+}
+
+func (s *BuzzerServer) ParseSong(_ context.Context, stringPayload *pb.SongStringPayload) (*pb.Song, error) {
+	reader := strings.NewReader(stringPayload.GetPayload())
+	parsed, err := car.ParseSongFile(reader)
+	if err != nil {
+		return &pb.Song{}, err
+	}
+
+	return exportSong(parsed), nil
+}
+
+func importSong(sng *pb.Song) car.Song {
+	song := car.Song{}
+	for _, n := range sng.GetNotes() {
+		song = append(song, car.Note{
+			Duration: float64(n.Duration),
+			Pitch:    float64(n.Pitch),
+		})
+	}
+	return song
+}
+
+func exportSong(song car.Song) *pb.Song {
+	var notes []pb.Note
+	for _, n := range song {
+		notes = append(notes, pb.Note{
+			Duration: float32(n.Duration),
+			Pitch:    float32(n.Pitch),
+		})
+	}
+	notesPointers := make([]*pb.Note, len(notes))
+	for i := range notes {
+		notesPointers[i] = &notes[i]
+	}
+	return &pb.Song{Notes: notesPointers}
+}
+
+func (s *BuzzerServer) AsyncPlaySong(_ context.Context, sng *pb.Song) (*empty.Empty, error) {
+	s.currentSong = importSong(sng)
+	s.signalFinish = make(chan struct{})
+	go func() {
+		err := s.buzz.PlaySong(s.currentSong, s.signalFinish)
+		if err != nil {
+			log.Println("Error at playing song: ", err)
+		}
+		s.signalFinish = nil
+		s.currentSong = car.Song{}
+		s.isClosing = false
+	}()
+	return nil, nil
+}
+
+func (s *BuzzerServer) DoesSongStillPlay(context.Context, *empty.Empty) (*pb.SongStatus, error) {
+	return &pb.SongStatus{
+		IsPlaying: s.currentSong != nil,
+	}, nil
+}
+
+func (s *BuzzerServer) StopSong(context.Context, *empty.Empty) (*empty.Empty, error) {
+	if s.signalFinish != nil {
+		if s.isClosing == false {
+			s.isClosing = true
+			close(s.signalFinish)
+		}
+	}
+	return nil, nil
+}
+
+func (s *BuzzerServer) GetSong(context.Context, *empty.Empty) (*pb.Song, error) {
+	return exportSong(s.currentSong), nil
+}
