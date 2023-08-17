aea959c32403dd3f1c8030f8575e617d91c318f5
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-08-17 15:13:55 +0200

    Rename "CreateADC" to "NewADC" in ADC.go
    The updated naming aligns better with the established Go language conventions, which typically prefer 'New' over 'Create' for function names. This renaming provides greater uniformity and readability within the code.

---
+++ Car/Camera.go
@@ -0,0 +1,23 @@
+package Car
+
+import (
+	"github.com/tractus/piCamera"
+)
+
+type Camera struct {
+	cam *piCamera.PiCamera
+}
+
+func NewCamera() (*Camera, error) {
+	cam, err := piCamera.New(nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	err = cam.Start()
+	defer cam.Stop()
+	return &Camera{cam: cam}, err
+}
+
+func (c *Camera) GetFrame() ([]byte, error) {
+	return c.cam.GetFrame()
+}
