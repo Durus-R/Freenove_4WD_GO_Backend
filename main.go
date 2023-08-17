eafeab6f9d417966ea6e4e5542d8753fb1a9c6b4
Rene Regensbogen <68396806+Durus-R@users.noreply.github.com>
2023-08-17 15:12:51 +0200

    Implement gRPC server functionality in main.go
    The main.go file has been updated to implement a gRPC server. Additional imports have been added to enable server functionality. A series of protocol buffers (protobufs) service registrations have been added such as Ultrasonic, ADC, Camera, Buzzer, Car, LED, Motor, Servo. Each service registration then has the associated checks included for proper initialization. These changes are vital for enabling communication between the back-end and front-end of the app using gRPC protocol. They allow different services and message types to be sent and received over a network, providing key functionality for the application.

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	car "Freenove_4WD_GO_Backend/Car"
+	pb "Freenove_4WD_GO_Backend/dist/proto"
+	"Freenove_4WD_GO_Backend/rpc"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 	"os/user"
 )
 
@@ -11,4 +16,54 @@ func main() {
 	if usr.Name != "root" {
 		log.Fatal("Please restart this system with root access")
 	}
+
+	s := grpc.NewServer()
+	pb.RegisterUltrasonicServer(s, &rpc.UltrasonicServer{
+		Ultrasonic: car.NewUltrasonic(),
+	})
+
+	pb.RegisterADCServer(s, &rpc.ADCServer{
+		ADC: car.NewADC(),
+	})
+
+	cam, err := car.NewCamera()
+	if err != nil {
+		log.Fatal("Failed to create Camera: ", err)
+	}
+	pb.RegisterCameraServer(s, &rpc.CameraServer{
+		Camera: cam,
+	})
+
+	buzz, err := car.NewBuzzer()
+	if err != nil {
+		log.Fatal("Failed to create buzzer: ", err)
+	}
+	pb.RegisterBuzzerServer(s, &rpc.BuzzerServer{Buzz: buzz})
+
+	pb.RegisterCarServer(s, &rpc.CarServer{})
+
+	pb.RegisterLEDServer(s, &rpc.LEDServer{LED: car.NewRGBStrip()})
+
+	mc, err := car.NewMotorController()
+	if err != nil {
+		log.Fatal("Cannot create Motor, ", err)
+	}
+	pb.RegisterMotorServer(s, &rpc.MotorServer{
+		MC: mc,
+	})
+
+	pb.RegisterServoServer(s, &rpc.ServoServer{
+		MC: mc,
+	})
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal("Failed to listen: ", err)
+	}
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatal("Error at serving: ", err)
+	}
+
 }
